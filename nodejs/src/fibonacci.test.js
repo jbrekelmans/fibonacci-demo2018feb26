6f515f671e803290fd3514ec87dd2d79ac32b8c2
Jasper Brekelmans <jasperbrekelmans@gmail.com>
2019-02-26 06:12:42 +1100

    fix mocha perms

--- nodejs/src/fibonacci.test.js
+++ nodejs/src/fibonacci.test.js
@@ -1,18 +1,18 @@
-const assert = require('assert')
+const assert = require('assert');
 
-const fibonacci = require('./fibonacci')
+const fibonacci = require('./fibonacci');
 
 describe('fibonacci', function () {
   it('has the correct base case (1)', function () {
-    const result = fibonacci(0n)
-    assert.strictEqual(result, 0n)
-  })
+    const result = fibonacci(0n);
+    assert.strictEqual(result, 0n);
+  });
   it('has the correct base case (2)', function () {
-    const result = fibonacci(1n)
-    assert.strictEqual(result, 1n)
-  })
+    const result = fibonacci(1n);
+    assert.strictEqual(result, 1n);
+  });
   it('correctly recurses', function () {
-    const result = fibonacci(3n)
-    assert.strictEqual(result, 2n)
-  })
-})
+    const result = fibonacci(3n);
+    assert.strictEqual(result, 2n);
+  });
+});
--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,21 +1,3 @@
-/**
- * Copyright 2018, Google, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-// [START app]
 const express = require('express');
 
 const fibonacci = require('./fibonacci');
@@ -43,7 +25,6 @@ app.get('/healthcheck', (req, res) => {
   res.status(200).send(`I'm fine!`);
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
@@ -52,6 +33,3 @@ process.on('SIGINT', function () {
   console.log(`Server stopping because SIGINT was received...`);
   server.close();
 });
-// [END app]
-
-module.exports = app;
