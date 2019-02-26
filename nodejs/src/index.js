6e4335701af9aac46c4d8b8ee6ad25921b62e696
Jasper Brekelmans <jasperbrekelmans@gmail.com>
2019-02-26 17:54:16 +1100

    improve validation

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -6,17 +6,19 @@ const app = express();
 
 app.get('/', (req, res) => {
   let n
-  let valid = false;
+  let valid;
   try {
     n = BigInt(req.query.n);
-    valid = true;
   } catch (error) {
+    res.status(400).type('text').send('Query string parameter "n" is missing or invalid');
+    return;
   }
   if (n < 0n) {
-    valid = false;
+    res.status(400).type('text').send('Query string parameter "n" must represent a non-negative integer');
+    return;
   }
-  if (!valid) {
-    res.status(400).type('text').send('Query string parameter "n" is missing or invalid');
+  if (n > 999n) {
+    res.status(400).type('text').send('Query string parameter "n" must be at most 999');
     return;
   }
   const result = fibonacci(n);
@@ -25,11 +27,11 @@ app.get('/', (req, res) => {
   if (nMod10 === 1n) suffix = 'st';
   else if (nMod10 === 2n) suffix = 'nd'
   else if (nMod10 === 3n) suffix = 'rd';
-  res.status(200).send(`The ${n}${suffix} Fibonacci number is ${result}!`);
+  res.status(200).type('text').send(`The ${n}${suffix} Fibonacci number is ${result}!`);
 });
 
 app.get('/healthcheck', (req, res) => {
-  res.status(200).send(`I'm fine!`);
+  res.status(200).type('text').send(`I'm fine!`);
 });
 
 const PORT = process.env.PORT || 8080;
