98547cec2be3b87c868dc681ecb2a891fab35157
Jasper Brekelmans <jasperbrekelmans@gmail.com>
2019-02-26 11:09:42 +1100

    iterative fibonacci

--- nodejs/src/fibonacci.js
+++ nodejs/src/fibonacci.js
@@ -1,23 +1,23 @@
-function fibonacci (n) {
-  if (n > 1n) {
-    return fibonacci(n - 1n) + fibonacci(n - 2n);
-  }
-  return n === 0n ? 0n : 1n;
-}
-
 // function fibonacci (n) {
-//   if (n === 0n) {
-//     return 0n;
+//   if (n > 1n) {
+//     return fibonacci(n - 1n) + fibonacci(n - 2n);
 //   }
-//   let prev = 0n;
-//   let cur = 1n;
-//   while (n > 1n) {
-//     const next = prev + cur;
-//     prev = cur;
-//     cur = next;
-//     n--;
-//   }
-//   return cur;
+//   return n === 0n ? 0n : 1n;
 // }
+
+function fibonacci (n) {
+  if (n === 0n) {
+    return 0n;
+  }
+  let prev = 0n;
+  let cur = 1n;
+  while (n > 1n) {
+    const next = prev + cur;
+    prev = cur;
+    cur = next;
+    n--;
+  }
+  return cur;
+}
 
 module.exports = fibonacci;
